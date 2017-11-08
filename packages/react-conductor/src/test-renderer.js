f9f2490f1e571e664d78ebe54e166539af103886
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2017-11-08 14:04:57 +0100

    Add comment about test renderer’s purpose

--- packages/react-conductor/src/test-renderer.js
+++ packages/react-conductor/src/test-renderer.js
@@ -1,6 +1,15 @@
 import createElement from './utils/create-element';
 import ElectronRenderer from './reconciler';
 
+/**
+ * TODO: the test renderer shouldn't actually render to Electron elements. It's
+ * supposed to help future users test their applications, not help testing this
+ * library.
+ *
+ * We'll need to switch all our tests to using the actual renderer, and change
+ * this one to the way it's done in React (and move it to a separate package
+ * probably)
+ */
 function render(element, launchInfo) {
   const container = createElement('ROOT');
   const node = ElectronRenderer.createContainer(container);
