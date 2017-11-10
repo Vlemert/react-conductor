fe836e88a32f9fc3f2ca1eff7d173cb7b6dc9977
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2017-11-10 17:20:55 +0100

    Move / rename render test

--- packages/react-conductor/src/render/__tests__/render.jsx
+++ packages/react-conductor/src/render/__tests__/render.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { app, BrowserWindow } from 'electron';
 
-import { Window } from '../index';
-import render from '../render';
+import { Window } from '../../index';
+import render from '../index';
 
 jest.mock('electron');
 
 /**
  * We should probably reorganize the tests. This no longer tests `Root`.
  */
-describe('Root', () => {
+describe('render', () => {
   beforeEach(() => {
     BrowserWindow.mockReset();
     app.once = jest.fn();
@@ -25,7 +25,7 @@ describe('Root', () => {
     return renderPromise;
   }
 
-  test('is rendered as the root of the element tree', async () => {
+  test('returns the component instance of the root element passed to it', async () => {
     class Application extends React.Component {
       render() {
         return <Window />;
